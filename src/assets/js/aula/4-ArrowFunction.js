473182ea0e76ded3c3a1a1a2272b435555da7936
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-05-23 21:51:23 -0300

    add funcao, arrowfunction

---
+++ src/assets/js/aula/4-ArrowFunction.js
@@ -0,0 +1,27 @@
+// var nome = 'Gustavo';
+
+
+// sem parametro faz sempre a mesma coisa
+// function converterMaiuscula (){
+//     return nome.toUpperCase();
+// }
+
+// console.log(converterMaiuscula());
+
+// function converterMaiuscula(texto){
+//     return texto.toUpperCase();
+
+// }
+
+// console.log(converterMaiuscula('Edmar'));
+
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+function media2numeros (n1, n2){
+    var resultado = soma(n1, n2)/2
+    return resultado;
+}
+
+console.log(media2numeros(1232313, 12332131));
